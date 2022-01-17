ec7f41f167f4b878b74d749374089b414deeb040
dira2912 <dira2912@gmail.com>
2022-01-17 08:42:24 -0600

    Ejercicios git para uso de funciones en JS

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,8 @@
+//PEDES IN TERRA AD SIDERA VISUS
+
+function payment(transactions, total){
+    var transactions = parseInt(prompt("Number of transactions"));
+    var total = parseInt(prompt("Total cost of payments"));
+    alert("The final payment is: " + ((transactions*3)+total)*1.01);
+}
+payment();
